e99dba20f4bf069e8108fae256f2549039d903ca
Vasiliy <30378728+vprotcykovich@users.noreply.github.com>
2018-04-24 23:30:00 +0300

    Add files via upload

---
+++ day7-js-1/simple-cache.js
@@ -0,0 +1,16 @@
+var cache = {
+	limit : 100,
+	stack: {}, //Cache stack
+	get: function(id){ //Gets cache if found
+		return (typeof(this.stack[id]) != 'undefined') ? this.stack[id] : false;
+	},
+	set: function(data,id){ //Sets new data (key/value couple)
+        if(Object.keys(this.stack).length < 100) {
+            this.stack[id] = data
+        }
+	},
+	remove: function(id){//Removes cache for identifier
+		if(typeof(this.stack[id]) != 'undefined')
+			delete this.stack[id];
+	},
+};
