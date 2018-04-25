3c84914bcdfa9344dc86d06ed6a422fad621f2ae
Vasiliy <30378728+vprotcykovich@users.noreply.github.com>
2018-04-25 20:30:53 +0300

    Update simple-cache.js

--- day7-js-1/simple-cache.js
+++ day7-js-1/simple-cache.js
@@ -1,13 +1,35 @@
 var cache = {
-	limit : 100, //Cache size limit
+	limit : 100,
 	stack: {}, //Cache stack
+	dataObject : {
+		storage: '', 
+		freq: 0,
+		set: function(storage) {
+			this.storage = storage;
+			return this;
+		}
+	},
 	get: function(id){ //Gets cache if found
-		return (typeof(this.stack[id]) != 'undefined') ? this.stack[id] : false;
+		if(typeof(this.stack[id].storage) != 'undefined') {
+			this.stack[id].freq++;
+			return this.stack[id].storage;
+		}
+		return false;
 	},
 	set: function(data,id){ //Sets new data (key/value couple)
-        	if(Object.keys(this.stack).length < 100) {
-		    this.stack[id] = data
+        if(Object.keys(this.stack).length < 100) {
+            this.stack[id] = new this.dataObject.set(data);
+            return;
+        }
+        //Looks for the key/value couple with the lowest frequency and replaces with a new couple
+        let leastFreq = Object.keys(this.stack)[0];
+        for(key in this.stack) {
+        	if(key.freq < leastFreq) {
+        		leastFreq = key.freq;
         	}
+        }
+        delete this.stack[leastFreq];
+        this.set(data,id);
 	},
 	remove: function(id){//Removes cache for identifier
 		if(typeof(this.stack[id]) != 'undefined')
