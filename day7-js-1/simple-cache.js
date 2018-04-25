aa0fc2d25fcb8737d6ce58b94fcc3b9a8f097fc7
Vasiliy <30378728+vprotcykovich@users.noreply.github.com>
2018-04-25 20:33:45 +0300

    Update simple-cache.js

--- day7-js-1/simple-cache.js
+++ day7-js-1/simple-cache.js
@@ -17,19 +17,19 @@ var cache = {
 		return false;
 	},
 	set: function(data,id){ //Sets new data (key/value couple)
-        if(Object.keys(this.stack).length < 100) {
-            this.stack[id] = new this.dataObject.set(data);
-            return;
+        	if(Object.keys(this.stack).length < 100) {
+            		this.stack[id] = new this.dataObject.set(data);
+            	return;
         }
         //Looks for the key/value couple with the lowest frequency and replaces with a new couple
-        let leastFreq = Object.keys(this.stack)[0];
-        for(key in this.stack) {
-        	if(key.freq < leastFreq) {
-        		leastFreq = key.freq;
+        	let leastFreq = Object.keys(this.stack)[0];
+        	for(key in this.stack) {
+        		if(key.freq < leastFreq) {
+        			leastFreq = key.freq;
+        		}
         	}
-        }
-        delete this.stack[leastFreq];
-        this.set(data,id);
+        	delete this.stack[leastFreq];
+        	this.set(data,id);
 	},
 	remove: function(id){//Removes cache for identifier
 		if(typeof(this.stack[id]) != 'undefined')
