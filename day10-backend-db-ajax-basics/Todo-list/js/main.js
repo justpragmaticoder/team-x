6fbc6e2f1fdf19977e49a6ca473944547a3d2def
Vasiliy <30378728+vprotcykovich@users.noreply.github.com>
2018-05-03 13:50:51 +0300

    Update main.js

--- day10-backend-db-ajax-basics/Todo-list/js/main.js
+++ day10-backend-db-ajax-basics/Todo-list/js/main.js
@@ -1,6 +1,7 @@
-var user = localStorage.getItem("name");
+const user = localStorage.getItem("name");
 console.log(user);
-$(document).ready(function(){
+$(document).ready(function () {
+    $('#main_field').focus();
     $.ajax({
         url: '/show',
         type: 'post',
@@ -9,89 +10,78 @@ $(document).ready(function(){
         data: JSON.stringify({
             user: user
         }),
-        success: function(res) {
+        success: function (res) {
             for (var i = 0; i < res.length; i++) {
                 $('.list-group').append('<li id="' + res[i].id + '"class="list-group-item">' + res[i].task + '<span onclick="del(this.parentElement.id)"class="badge">x</span><a href="#updateModal" onclick="upd(this.parentElement.id)"  class="badge">edit</a> </li>');
             }
         }
     });
-	//Makes focus on the task input field as soon as the page has fully loaded.
-	/*$(function() {
-		$('#main_field').focus();
 
-		var storageLength = localStorage.length;
-      	if(storageLength > 0){
-        	for(var i = 1; i <= storageLength; i++){
-          		value = localStorage.getItem(i);
-          		$(".list-frame").append(value);
-        	}
-     	}
-	});*/
-	
-	//Adds a new task to the todo list.
-	$('#main_field').keyup(function(e){
-		var taskText = e.target.value;
-    	if(e.keyCode == 13 && taskText.length > 0) {
-                $.ajax({
-                    url: "/create",
-                    dataType: 'json',
-                    contentType: "application/json",
-                    type: 'post',
-                    data: JSON.stringify({
-                        newTask: taskText,
-						user: user
-                    }),
-                    success: function(res) {
-                        $('.list-group').prepend('<li id="' + res[0].id + '"class="list-group-item">' + res[0].task + '<span onclick="del(this.parentElement.id)"class="badge">x</span><a href="#updateModal" onclick="upd(this.parentElement.id)"  class="badge">edit</a> </li>');
-                    },
-                    error: function() {
-                        alert('task no add!');
-                    }
-                });
-            }
-        });
-
-	//Hides the last added task when the list is already full.
-   	$('.list-frame').bind('DOMNodeInserted DOMNodeRemoved', function(event) {
-    	if($(this).outerHeight() >= $('.task-container').innerHeight()) {
-    		$('.list-frame .single-task:last').hide();
-    	}
-    	if($(this).outerHeight() < $('.task-container').innerHeight()) {
-    		$('.list-frame .single-task:hidden:first').show();
-    	}
-	});
-
-
-	$('.input-field input[type=checkbox]').on('click', function() {
-		if($(this).prop('checked')) {
-			$('.list-frame input[type=checkbox]').prop('checked', 'true');
-			return
-		}
-		$('.list-frame input[type=checkbox]').removeProp('checked');
-	});
-
-
-	$('.list-frame').on("dblclick", ".single-task .task-text", function () {
-    	$(this).replaceWith('<input type="text" class=task-text>');
-
-    	$('.single-task input[type=text]').keyup(function(e){
-    		var newText = e.target.value;
-    		if(e.keyCode == 13 && newText.length > 0) {
-    			$(this).replaceWith('<p class="task-text">' + newText + '</p>');
-    		}
-    	});
+    //Adds a new task to the todo list.
+    $('#main_field').keyup(function (e) {
+        var taskText = e.target.value;
+        if (e.keyCode == 13 && taskText.length > 0) {
+            $(this).val('');
+            $.ajax({
+                url: "/create",
+                dataType: 'json',
+                contentType: "application/json",
+                type: 'post',
+                data: JSON.stringify({
+                    newTask: taskText,
+                    user: user
+                }),
+                success: function (res) {
+                    $('.list-group').prepend('<li id="' + res[0].id + '"class="list-group-item">' + res[0].task + '<span onclick="del(this.parentElement.id)"class="badge">x</span><a href="#updateModal" onclick="upd(this.parentElement.id)"  class="badge">edit</a> </li>');
+                },
+                error: function () {
+                    alert('The task is not added!');
+                }
+            });
+        }
     });
 
-	//Deletes the task when user clicks on the closing cross.
-    $('.list-frame').on('click', '.single-task .delete', function(){
-    	var keyName = $(this).parent().attr('elemId');
-    	localStorage.removeItem(keyName);
-  		$(this).parent().remove();
-  	});
+    //Hides the last added task when the list is already full.
+    $('.list-frame').bind('DOMNodeInserted DOMNodeRemoved', function (event) {
+        if ($(this).outerHeight() >= $('.task-container').innerHeight()) {
+            $('.list-frame .single-task:last').hide();
+        }
+        if ($(this).outerHeight() < $('.task-container').innerHeight()) {
+            $('.list-frame .single-task:hidden:first').show();
+        }
+    });
 
+
+    $('.input-field input[type=checkbox]').on('click', function () {
+        if ($(this).prop('checked')) {
+            $('.list-frame input[type=checkbox]').prop('checked', 'true');
+            return
+        }
+        $('.list-frame input[type=checkbox]').removeProp('checked');
+    });
+
+
+    $('.list-frame').on("dblclick", ".single-task .task-text", function () {
+        $(this).replaceWith('<input type="text" class=task-text>');
+
+        $('.single-task input[type=text]').keyup(function (e) {
+            var newText = e.target.value;
+            if (e.keyCode == 13 && newText.length > 0) {
+                $(this).replaceWith('<p class="task-text">' + newText + '</p>');
+            }
+        });
+    });
+
+    //Deletes the task when user clicks on the closing cross.
+    $('.list-frame').on('click', '.single-task .delete', function () {
+        var keyName = $(this).parent().attr('elemId');
+        localStorage.removeItem(keyName);
+        $(this).parent().remove();
+    });
 
 
 });
+
 function del(id) {
     $.ajax({
 
@@ -100,15 +90,15 @@ function del(id) {
         contentType: "application/json",
         type: 'post',
         beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", 'Bearer '+ localStorage.getItem("token"));
+            xhr.setRequestHeader("Authorization", 'Bearer ' + localStorage.getItem("token"));
         },
         data: JSON.stringify({
             delete: id
         }),
-        success: function() {
+        success: function () {
             $('#' + id).remove();
         },
-        error: function() {
+        error: function () {
             alert("deleting error");
         }
     })
